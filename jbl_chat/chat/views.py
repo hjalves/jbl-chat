1bb2623f69a8c57a1d3f6d7aadc8bffb51e9234a
Humberto Alves <hjalves@live.com>
2025-03-15 18:47:04 +0000

    Add basic layout structure with placeholder pages

--- jbl_chat/chat/views.py
+++ jbl_chat/chat/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_world(request):
+    return render(request, 'chat/mock_page.html')
+
+
+def mock_page(request):
+    return render(request, 'chat/mock_page.html')
+
+
+def mock_messages(request):
+    return render(request, 'chat/mock_messages.html')
